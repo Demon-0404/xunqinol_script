33971415693627bcd2cf3c2d41c9e065b246a616
Demon-0404 <zlj15634398726@163.com>
2026-09-01 01:10:27 +0800

    feat: 新增地图数据/宏/天渊战斗模板/UI启动脚本
    - map_data: 地图名匹配数据(maps.json + 各图b64模板)
    - macros: 楼层宏
    - templates/tianyuan: 天渊自动/手动/回合按钮模板
    - _launch_ui.bat: UI启动脚本

---
+++ _launch_ui.bat
@@ -0,0 +1,5 @@
+@echo off
+start "XunQin-1" "D:/Setup_and_Downloads/Setup/python3.12.4/python.exe" "E:\DATA\xunqinol_script\main.py"
+start "XunQin-2" "D:/Setup_and_Downloads/Setup/python3.12.4/python.exe" "E:\DATA\xunqinol_script\main.py"
+start "XunQin-3" "D:/Setup_and_Downloads/Setup/python3.12.4/python.exe" "E:\DATA\xunqinol_script\main.py"
+start "XunQin-4" "D:/Setup_and_Downloads/Setup/python3.12.4/python.exe" "E:\DATA\xunqinol_script\main.py"
